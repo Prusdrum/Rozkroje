51fbc3c8414e71c24c911a29e7825c595366382e
MAKINGWAVES\mateusz.prusaczyk <mateusz.prusaczyk@makingwaves.com>
2017-07-08 15:05:17 +0200

    added start script

---
+++ build/start_app.js
@@ -0,0 +1,18 @@
+const exec = require('child_process').exec;
+const child = exec('npm run client:build',
+    (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+});
+
+const child2 = exec('node ./bin/www',
+    (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+});
