e804df884fd960a68bd3bf23540463abc4981616
= <mateusz.prusaczyk@gmail.com>
2017-10-09 16:12:08 +0200

    version 0.6.0

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -17,7 +17,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'es2015', 'react'],
+                        presets: ['es2015', 'react', 'env'],
                         plugins: ['transform-object-rest-spread']
                     }
                 }
